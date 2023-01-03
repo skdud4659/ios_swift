7a4347444833c250866c7b8f74641284e152630f
skdud4659 <skdud4659@gmail.com>
2023-01-03 12:24:14 +0900

    12. TableView(wz storyboard)

--- 12_TableView/TabelView/Models/DataManager.swift
+++ 12_TableView/TabelView/Models/DataManager.swift
@@ -32,7 +32,6 @@ class DataManager {
     func updateMovieData() {
         let movie = Movie(movieImage: UIImage(named: "spiderman2.png"), movieName: "스파이더맨2", movieDescription: "스파이더맨 시즌2")
         movieDataArray.append(movie)
-        print(movieDataArray)
     }
     
 }
